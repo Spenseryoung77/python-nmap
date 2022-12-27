fa19fc6c7d6ace9d105556ed63e7ccf1a7db863a
Spenseryoung77 <121503975+Spenseryoung77@users.noreply.github.com>
2022-12-27 15:00:21 +0000

    Script.py
    Non commen...

---
+++ Main.py
@@ -0,0 +1,44 @@
+#nmap scan script by Spenser young
+#Lun. 26 Dec 2022 à 23h20
+#pip3 install colorama
+#pip3 install python-nmap
+#------------------$pęñ$ę®---------------#
+import nmap
+import colorama
+import Fixe from colorama
+#Style for script
+                                                  
+print(" _ __   _ __ ___    __ _  _ __") 
+print("| '_ \ | '_ ` _ \  / _` || '_ \   ")
+print("| | | || | | | | || (_| ||     \\")
+print("| | | || | | | | ||     |      /")
+print("|_| |_||_| |_| |_| \__,_|| ._ / ")
+print("                         | |  ")  
+print("                         |_| by Spenser Young v¹ " )
+#End
+#Code
+if en == 1 :
+          port()
+#    if enter == 2 :
+#    host()
+#    if enter == 3 :
+#    Ip()
+def port() :              
+ input("\n\n1. Scan PORT\n2. Scan HOST")
+def main () :
+    en
+                                   
+     print("______________________________________________________") 
+     print("                     PORT SCANNER         ")
+     print ("\n")
+     ip = input("Enter IP: ")
+     ports = input ("Enter PORT: ")
+     ports =  nmap.PortScanner()
+    #ports.scan(ip, portss)
+    print
+print("_____________________________________________________") 
+
+    
+if __name__  ==  '__main__' :
+    main()
+    
